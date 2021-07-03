80fb1140719f4363812f375fce0915bea5846c96
Manisha-2447 <73066130+Manisha-2447@users.noreply.github.com>
2021-07-03 20:35:22 +0530

    Add files via upload

---
+++ onlinecourseregistration22/admin/includes/header.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(0);
+?>
+
+    <!-- HEADER END-->
+    <div class="navbar navbar-inverse set-radius-zero">
+        <div class="container">
+            <div class="navbar-header">
+                 <a class="navbar-brand" href="#" style="color:#fff; font-size:18px; line-height:24px; ">
+
+                   Online Course Registration
+                </a>
+
+            </div>
+
+                
+        </div>
+            </div>
+      
